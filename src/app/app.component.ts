8b0dd63f24e48900fa0f0b544e2962f972feff19
vishal <vishaldivekar12@gmail.com>
2020-09-07 16:12:09 +0530

    updates to deploy to heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'space-x';
+  title = 'SpaceX-Program';
+
+  yearList = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020];
+
+  selectedYear = '';
+  selectedLaunch = null;
+  selectedLanding = null;
+  dataList = [];
+  hasLoader = false;
+  constructor(private http: HttpClient) {
+    this.getSpaceXData();
+  }
+
+  getSpaceXData() {
+    this.dataList = [];
+    this.hasLoader = true;
+    this.http.get(`https://api.spaceXdata.com/v3/launches?limit=100&launch_success=${this.selectedLaunch || ''}&land_success=${this.selectedLanding || ''}&launch_year=${this.selectedYear}`).subscribe((res: any) => {
+      this.dataList = res;
+      this.hasLoader = false;
+    });
+  }
+
 }
